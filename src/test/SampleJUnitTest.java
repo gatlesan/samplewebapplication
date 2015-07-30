f79234f1abb7a2cb2106e72068aca70823ac187c
sgatlewa <gatlewar.santa544@gmail.com>
2015-07-30 09:03:41 -0700

    8-commit

---
+++ src/test/SampleJUnitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleJUnitTest {
+
+	@Test
+	public void test() {
+		assertEquals("test", "test");
+	}
+
+}
